413301e00ed924a0adac8ad2c4a4dda703287c35
NguyenNhuSang <63133602+NguyenNhuSang@users.noreply.github.com>
2020-06-22 00:10:50 +0700

    pract

---
+++ lab01/lab01/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public static class Sum
+    {
+        public static int calcSumWithReturn(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        public static void calcSumNoReturn(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine("Sum:" + sum);
+            Console.ReadLine();
+        }
+    }
+}
